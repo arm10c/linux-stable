709a808a5c2da233b881950a454a93d86431699f
JungHee Son <aceofall@naver.com>
2013-12-01 00:07:44 +0900

    Kernel compile error fix.
    - Code comments make some compile errors. so fix it.

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -37,7 +37,9 @@
  * TASK_SIZE - the maximum size of a user space task.
  * TASK_UNMAPPED_BASE - the lower boundary of the mmap VM area
  */
+/*
 // PAGE_OFFSET: 0xC0000000
+*/
 #define PAGE_OFFSET		UL(CONFIG_PAGE_OFFSET)
 /*
 // ARM10C 20131102
